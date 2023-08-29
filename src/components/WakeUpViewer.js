effdb61cb8cdc9d3faf1c0630b267bb0cb2237af
bathan <side_nathan@hotmail.com>
2023-08-29 19:49:25 -0400

    Add WakeUpViewer component

---
+++ src/components/WakeUpViewer.js
@@ -0,0 +1,15 @@
+const WakeUpViewer = ({wakeUpTimes}) => {
+    if (wakeUpTimes.length === 0) {
+        return (
+            <div>Write your bedtime!</div>
+        )
+    } else {
+        return(
+            <div>
+                <p>Your wakeup times are {wakeUpTimes.join(", ")}</p>
+            </div>
+        )
+    }
+}
+
+export default WakeUpViewer
