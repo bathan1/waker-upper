3826ebea122e14928bb2103fdd487d0ff9aede85
bathan <side_nathan@hotmail.com>
2023-09-02 12:46:17 -0400

    Properly add signupform.js

---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+const SignupForm = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    });
+
+    const handleChange = (event) => {
+        const {name, value}  = event.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch('/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            });
+            if (response.status === 201) {
+                console.log("Registration success");
+            } else {
+                console.log("Registration failed :(");
+            }
+        } catch (error) {
+            console.log('Error:', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                placeholder="Username"
+            />
+            <br></br>
+            <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="Password"
+            />
+            <br></br>
+            <button type="submit">Sign up</button>
+        </form>
+    );
+};
+
+export default SignupForm
