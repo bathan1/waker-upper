124f3b1693885a0a55dfab28229472b1199cec54
bathan <side_nathan@hotmail.com>
2023-09-02 12:11:05 -0400

    Successfully add React build to Express app

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
-// Begin Express app
+const path = require('path');
 const app = express();
 
-const mongoURI = 
+const buildDir = '../client/build';
+
+app.use(express.static(path.join(__dirname, buildDir)));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
